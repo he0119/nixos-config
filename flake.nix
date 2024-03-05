a21cade8a96a89791b072bedbbe7ea4436217750
uy_sun <hmy0119@gmail.com>
2024-03-05 15:53:52 +0800

    feat: add home manager

--- flake.nix
+++ flake.nix
@@ -7,36 +7,57 @@
 
     vscode-server.url = "github:nix-community/nixos-vscode-server";
 
-    alejandra.url = "github:kamadorueda/alejandra/3.0.0";
-    alejandra.inputs.nixpkgs.follows = "nixpkgs";
+    # nix 文件格式化工具
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    vscode-server,
+    home-manager,
     alejandra,
+    vscode-server,
     ...
   } @ inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
       system = "x86_64-linux";
       modules = [
+        # 这里导入之前我们使用的 configuration.nix，
+        # 这样旧的配置文件仍然能生效
+        ./configuration.nix
+
+        # 将 home-manager 配置为 nixos 的一个 module
+        # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.uy_sun = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+
         {
           environment.systemPackages = [alejandra.defaultPackage.${system}];
         }
 
         vscode-server.nixosModules.default
-        ({
-          config,
-          pkgs,
-          ...
-        }: {
+        {
           services.vscode-server.enable = true;
-        })
-
-        # 这里导入之前我们使用的 configuration.nix，
-        # 这样旧的配置文件仍然能生效
-        ./configuration.nix
+        }
       ];
     };
   };
