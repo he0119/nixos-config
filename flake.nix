df3976a36332c5748816e13cf47f0651f0a46f4b
uy_sun <hmy0119@gmail.com>
2024-03-05 17:13:00 +0800

    feat: try modularize configuration

--- flake.nix
+++ flake.nix
@@ -5,14 +5,6 @@
     # NixOS 官方软件源，这里使用 nixos-23.11 分支
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
-
-    # nix 文件格式化工具
-    alejandra = {
-      url = "github:kamadorueda/alejandra/3.0.0";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
       # The `follows` keyword in inputs is used for inheritance.
@@ -21,44 +13,88 @@
       # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # remote-ssh 必须配置
+    vscode-server.url = "github:nix-community/nixos-vscode-server";
+
+    # nix 文件格式化工具
+    alejandra = {
+      url = "github:kamadorueda/alejandra/3.0.0";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # add git hooks to format nix code before commit
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     home-manager,
-    alejandra,
+    pre-commit-hooks,
     vscode-server,
     ...
-  } @ inputs: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem rec {
-      system = "x86_64-linux";
-      modules = [
-        # 这里导入之前我们使用的 configuration.nix，
-        # 这样旧的配置文件仍然能生效
-        ./configuration.nix
+  } @ inputs: let
+    inherit (self) outputs;
+    # Supported systems for your flake packages, shell, etc.
+    systems = [
+      "x86_64-linux"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    # Formatter for your nix files, available through 'nix fmt'
+    formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.alejandra);
 
-        # 将 home-manager 配置为 nixos 的一个 module
-        # 这样在 nixos-rebuild switch 时，home-manager 配置也会被自动部署
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.uy_sun = import ./home.nix;
+    # pre-commit hooks for nix code
+    checks = forAllSystems (
+      system: {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true; # formatter
+            deadnix.enable = true; # detect unused variable bindings in `*.nix`
+            statix.enable = true; # lints and suggestions for Nix code(auto suggestions)
+            prettier = {
+              enable = true;
+              excludes = [".js" ".md" ".ts"];
+            };
+          };
+        };
+      }
+    );
 
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
+    # NixOS configuration entrypoint
+    # Available through 'nixos-rebuild --flake .#your-hostname'
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main nixos configuration file <
+          ./nixos/configuration.nix
+          vscode-server.nixosModules.default
+          {
+            services.vscode-server.enable = true;
+          }
+        ];
+      };
+    };
 
-        {
-          environment.systemPackages = [alejandra.defaultPackage.${system}];
-        }
-
-        vscode-server.nixosModules.default
-        {
-          services.vscode-server.enable = true;
-        }
-      ];
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "uy_sun@nixos" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home-manager/home.nix
+        ];
+      };
     };
   };
 }
