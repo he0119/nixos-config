ec9ba08ed9d1f20768b2dfc413c2b5fffd35caa7
uy_sun <hmy0119@gmail.com>
2024-03-13 01:42:02 +0000

    feat: enable docker on miemie

--- hosts/miemie/default.nix
+++ hosts/miemie/default.nix
@@ -119,6 +119,9 @@ in {
     }
   ];
 
+  # Docker
+  virtualisation.docker.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
