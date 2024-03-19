0d7831b3f994f38a0f7d912bcd69950e7fbe60b6
uy_sun <hmy0119@gmail.com>
2024-03-19 19:21:03 +0800

    chore(hosts): 删除多余注释并翻译

--- hosts/work-305/default.nix
+++ hosts/work-305/default.nix
@@ -1,48 +1,26 @@
-{
-  myvars,
-  mylib,
-  pkgs,
-  ...
-}:
+{mylib, ...}:
 #############################################################
 #
-#  WSL - a NixOS running on Windows Subsystem for Linux
+#  Work-305 - 运行在 Proxmox Virtual Environment 上的 NixOS
 #
 #############################################################
 let
-  hostName = "work-305"; # Define your hostname.
-  # hostAddress = myvars.networking.hostAddress.${hostName};
+  hostName = "work-305";
 in {
   imports = mylib.scanPaths ./.;
 
   networking = {
     inherit hostName;
 
-    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-    # Configure network proxy if necessary
-    # proxy.default = "http://user:password@proxy:port/";
-    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-    # Enable networking
+    # 启用网络
     networkmanager.enable = true;
   };
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+  # 这个选项定义了你在这台机器上安装的第一个 NixOS 版本，用于维护与旧版本 NixOS 创建的应用数据（例如，数据库）的兼容性。
+  # 大多数用户在初始安装后，无论出于何种原因，都不应更改此值，即使你已经将系统升级到新的 NixOS 发行版。
+  # 此值不会影响从 Nixpkgs 版本中拉取你的包和操作系统，因此更改它不会升级你的系统。
+  # 此值低于当前 NixOS 发行版并不意味着你的系统过时，不受支持，或者有漏洞。
+  # 除非你已经手动检查了所有更改此值会对你的配置产生的影响，并相应地迁移了你的数据，否则不要更改此值。
+  # 更多信息，请参阅 `man configuration.nix` 或 https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion。
+  system.stateVersion = "23.11";
 }
